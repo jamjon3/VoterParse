ab004719233a9c755f2a3e03067fd00a892e5ed7
James Jones <jamjon3@gmail.com>
2012-04-08 19:38:41 -0400

    Removed a static option. Now all this is in the settings.ini

--- index.php
+++ index.php
@@ -8,21 +8,7 @@
         <?php
             include_once 'Import.php';
             $import = new Import();
-            $import->extractVoters("TestVoters.txt");
-            
-//            include_once 'Connection.php';
-//            include_once 'Parser.php';
-//            $parser = new Parser();
-//            $handle = @fopen("TestVoters.txt", "r");
-//            if ($handle) {
-//                while (($buffer = fgets($handle, 4096)) !== false) {
-//                    $parser->parse($buffer);
-//                }
-//                if (!feof($handle)) {
-//                    echo "Error: unexpected fgets() fail\n";
-//                }
-//                fclose($handle);
-//            }
+            $import->extractVoters();
         ?>
     </body>
 </html>
