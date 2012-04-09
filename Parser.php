c354ffd07d97ac4e6308a0d13b3603d9a2eb48b7
James Jones <jamjon3@gmail.com>
2012-04-08 22:58:46 -0400

    Added handling of delimiter separators. The default is still fixed width. This can be switched now in the INI file by specifying the importType and the importTemplate (refers to the section that contains the import values for each field).

--- Parser.php
+++ Parser.php
@@ -20,11 +20,21 @@ class Parser extends Connection {
         $this->sth = $this->dbh->prepare($this->settings['import']['importSQL']);   
     }
     public function parse($line) {
-        $prepareValues = array();
-        foreach ($this->settings['parser'] as $key => $value) {
-            $prepareValues = array_merge($prepareValues,array(
-                ":".$key => substr($line,$value[0],$value[1])
-            ));
+        $prepareValues = array();        
+        foreach ($this->settings[$this->settings['import']['importTemplate']] as $key => $value) {
+            switch ($this->settings['import']['importType']) {
+                case "DELIMITER": case "delimiter":
+                    $arr = explode($value[0], $line);
+                    $prepareValues = array_merge($prepareValues,array(
+                        ":".$key => $arr[$value[1]]
+                    ));
+                    break;
+                case "FIXED": case "fixed": default:
+                    $prepareValues = array_merge($prepareValues,array(
+                        ":".$key => substr($line,$value[0],$value[1])
+                    ));
+                    break;
+            }
         }
         $this->dbh->beginTransaction();        
         $this->sth->execute($prepareValues);
