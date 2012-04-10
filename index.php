30a47ad205f18bfa78e230255bdb9da3019b6246
James Jones <jamjon3@gmail.com>
2012-04-10 10:20:43 -0400

    Wiring some JQuery, JQuery UI, Datatables and Backbone.js into the web frontend.

--- index.php
+++ index.php
@@ -2,9 +2,31 @@
 <html>
     <head>
         <meta http-equiv="Content-Type" content="text/html; charset=UTF-8">
-        <title></title>
+        <title>VoterParse</title>
     </head>
     <body>
+        <script type='text/javascript' src='https://www.google.com/jsapi'></script>
+        <script type="text/javascript">
+            if (window.google && window.google.load) {
+                google.load('jquery', '1.6.1');
+                google.load('jqueryui', '1.8.13');
+                google.setOnLoadCallback(function() {
+                    // $('body').prTabber();                    
+                });
+            } else {
+                // Fallback if Google can't load JQuery for us
+                document.write('<script type="text/javascript" src="jquery-ui-1.8.8/js/jquery-1.4.4.min.js"><\/script>');
+                document.write('<script type="text/javascript" src="jquery-ui-1.8.8/js/jquery-ui-1.8.8.custom.min.js"><\/script>');
+                window.onload = function() {
+                    $(document).ready(function () {
+                        // $('body').prTabber();
+                    });
+                };            
+            }
+        </script>        
+        <script type="text/javascript" src="https://ajax.aspnetcdn.com/ajax/jquery.dataTables/1.9.0/jquery.dataTables.min.js"></script>   
+        <script src="http://ajax.cdnjs.com/ajax/libs/underscore.js/1.1.4/underscore-min.js"></script>
+        <script src="http://ajax.cdnjs.com/ajax/libs/backbone.js/0.3.3/backbone-min.js"></script>        
         <?php
             include_once 'Import.php';
             include_once 'Services.php';
