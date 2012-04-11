cb9d6b6c274f3e1ded25b44a7bd68013ce10f072
James Jones <jamjon3@gmail.com>
2012-04-11 09:59:00 -0400

    Added a header comment here.

--- Connection.php
+++ Connection.php
@@ -1,14 +1,26 @@
 <?php
-
 /*
- * To change this template, choose Tools | Templates
- * and open the template in the editor.
- */
-
-/**
- * Description of Connection
+ * Providing for the database connection
+ * 
+ * This handles creating a PDO connection to the backend database
+ * 
+ * CONTACT: jamjon3@stpetegreens.org
+ * 
+ * LICENSE: This source file is free software, under the GPL v2 license, as supplied with this software.
  *
- * @author jamesjones
+ * This source file is distributed in the hope that it will be useful, but
+ * WITHOUT ANY WARRANTY; without even the implied warranty of MERCHANTABILITY
+ * or FITNESS FOR A PARTICULAR PURPOSE. See the license files for details.
+ *
+ * For details please refer to: http://www.stpetgreens.org
+ * 
+ * @package     jquery.ui.VoterParse.js
+ * @version     0.1
+ * @author      James Jones (www.stpetegreens.org)
+ * @since       04/11/2012
+ * @license     GPL v2
+ * @category    VoterParse
+ * @copyright   2011-2012 James Jones, all rights reserved.
  */
 class Connection {
     public $dbh;
