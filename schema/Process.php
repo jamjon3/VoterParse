2334d909451fdf02ca2c09328310170ccaf80ee4
jam <jam@jam-Aspire-5560>
2012-07-05 22:19:38 -0400

    Building out new schema classes

--- schema/Process.php
+++ schema/Process.php
@@ -15,11 +15,41 @@ include_once '../conf/Connection.php';
 class Process extends Connection {
     public $processId;
     public $script;
-    public $status;
+    public $statusId;
     
     public function __construct() {
         parent::__construct();
     }
+    public function getProcesses() {
+        $processes = array();
+        try {
+            $sth = $this->dbh->prepare("SELECT process_id AS processId,
+                script,
+                status_id AS statusId
+                FROM Process ORDER BY process_id");
+            $sth->execute();
+            $processes = $sth->fetchAll(PDO::FETCH_CLASS, 'Process');
+        } catch(PDOException $e) {
+            print $e->getMessage(); 
+        }        
+        return $processes;
+    }
+    public function get($processId) {
+        $processes = array();
+        try {
+            $sth = $this->dbh->prepare("SELECT process_id AS processId,
+                script,
+                status_id AS statusId
+                FROM Process WHERE process_id = :processId");
+            $sth->execute(array(':processId' => $processId));
+            $processes = $sth->fetchAll(PDO::FETCH_CLASS, 'Process');
+        } catch(PDOException $e) {
+            print $e->getMessage(); 
+        }        
+        return $processes[0];        
+    }
+        
+    
 }
 
 ?>
--- schema/Script.php
+++ schema/Script.php
@@ -21,6 +21,37 @@ class Script extends Connection {
     public function __construct() {
         parent::__construct();
     }
+    public function getScripts() {        
+        $processes = array();
+        try {
+            $sth = $this->dbh->prepare("SELECT script_id AS scriptId,
+                scriptType AS scriptType,
+                stateLabel AS stateLabel,
+                script AS script
+                FROM Script ORDER BY script_id");
+            $sth->execute();
+            $processes = $sth->fetchAll(PDO::FETCH_CLASS, 'Process');
+        } catch(PDOException $e) {
+            print $e->getMessage(); 
+        }        
+        return $processes;
+    }
+    public function get($scriptId) {
+        $scripts = array();
+        try {
+            $sth = $this->dbh->prepare("SELECT script_id AS scriptId,
+                scriptType AS scriptType,
+                stateLabel AS stateLabel,
+                script AS script
+                FROM Script WHERE script_id = :scriptId");
+            $sth->execute(array(':scriptId' => $scriptId));
+            $scripts = $sth->fetchAll(PDO::FETCH_CLASS, 'Script');
+        } catch(PDOException $e) {
+            print $e->getMessage(); 
+        }        
+        return $scripts[0];
+        
+    }
 }
 
 ?>
